3fe638f30c3f76f1c32f934db035af316c84b79d
AryanAg08 <aryangoyal90@outlook.com>
2023-07-03 23:15:15 +0530

    Day2

--- day2.py
+++ day2.py
@@ -1,8 +1,18 @@
-print("Welcome to the tip calculator!")
-total = float(input("Enter the total amount? "))
-PerTip = int(input("What percentage of tip do you want to give?"))
-members = int(input("enter the total members?"))
-tip = (PerTip * total) / 100
-totalamount = (total + tip) / members
-final = round(totalamount, 2)
-print(f"Each person has to pay {final}")
+## BILL SPLITTTER CODE ##
+
+# print("Welcome to the tip calculator!")
+# total = float(input("Enter the total amount? "))
+# PerTip = int(input("What percentage of tip do you want to give?"))
+# members = int(input("enter the total members?"))
+# tip = (PerTip * total) / 100
+# totalamount = (total + tip) / members
+# final = round(totalamount, 2)
+# print(f"Each person has to pay {final}")
+
+
+
+print("hello"[1]) # prints 2nd character from the string;
+
+
+# To get length we len function 
+print(len("hello world"))
